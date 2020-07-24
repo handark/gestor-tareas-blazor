fb81183b8da892c19923bb63a891541a7aaaa256
Jose Luis Orozco <handark@gmail.com>
2020-07-23 21:30:10 -0500

    ok

--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,11 @@ namespace GestorTareasBlazor
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSignalR().AddAzureSignalR(options =>
+            {
+                options.ServerStickyMode = 
+                    Microsoft.Azure.SignalR.ServerStickyMode.Required;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
